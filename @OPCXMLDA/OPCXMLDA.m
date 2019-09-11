ac472390222ba7b4683cf8c44b6d970ad88dbe44
pirmink <pirmink@gmail.com>
2019-09-11 14:59:49 +0200

    Added write function and typechecking
    Added the write function and also functions to check the type of given variables so only allowed variable types are sent to opc server

--- @OPCXMLDA/OPCXMLDA.m
+++ @OPCXMLDA/OPCXMLDA.m
@@ -3,7 +3,7 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
     %   Detailed explanation goes here
     
     properties
-        url = 'http://128.131.133.45:8080'
+        url = 'http://128.131.133.36:8080'
     end
     
     properties (SetAccess = private) % Read Only
@@ -11,7 +11,7 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
         operationID
         opctags
         opctypes
-        urls = struct('Reactor_10','http://128.131.133.36:8080','Reactor_20','http://128.131.133.37:8080','Reactor_2','http://128.131.133.45:8080')
+        urls = struct('Reactor_20','http://128.131.133.36:8080','Reactor_10','http://???','Reactor_2','http://128.131.133.45:8080')
         soapaction % is dynamically assigned by initialization
     end
     
@@ -19,46 +19,40 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
         options = weboptions()   
         operations = ["Browse" "Read" "Write"]
         soapxml = struct('Browse','browse.xml','Read','read.xml','Write','write.xml')
+        types = ["xsd:float"        "single";...
+                 "xsd:float"        "double";...
+                 "xsd:double"       "double";...
+                 "xsd:boolean"      "logical";...
+                 "xsd:int"          "int16";...
+                 "xsd:unsignedInt"  "uint16";...
+                 "xsd:string"       "string"]
+        
     end
 
     methods
         function obj = OPCXMLDA(obj) %Object initialization.
-            obj.operationID = obj.initOperationID()
+            obj.operationID = obj.initOperationID();
             for i = 1:length(obj.operations)
-                obj.operation.(obj.operations(i)) = obj.operations(i)
-                obj.soapaction.(obj.operations(i)) = xmlread(obj.soapxml.(obj.operations(i)))
+                obj.operation.(obj.operations(i)) = obj.operations(i);
+                obj.soapaction.(obj.operations(i)) = xmlread(obj.soapxml.(obj.operations(i)));
             end
         end
-        
-        % Set Action maybe not needed in the end if i make seperate
-        % functions for all operations
-        %         function set.operation(obj,operation)
-        %             if operationisValid(operation)
-        %                 obj.operation = operation
-        %                 %maybe i should make these changes individualy on requests
-        %                 obj.actionid = find(contains(obj.operations,operation))
-        %                 obj.actionxml = obj.soapaction(obj.actionid)
-        %                 obj.options = weboptions()
-        %                 obj.options.ContentType='xml'
-        %                 obj.headers = {'SOAPAction' strcat('http://opcfoundation.org/webservices/XMLDA/1.0/',obj.operation)}
-        %                 obj.options.HeaderFields=obj.headers
-        %             else
-        %                 error('Invalid Action')
-        %             end
-        %         end
         
         % Main Function definitions
         % here i make the functions for browse read and write requests
         
         function [value, type] = read(obj,ItemNameArg)
             %r = itemName
-            if (nargin == 1)
-                itemName = [""];
-            elseif (nargin == 2)
-                itemName = ItemNameArg;
-                fields = textscan(ItemNameArg,'%s','Delimiter','.');
+            if (nargin == 2)
+                try
+                    itemName = ItemNameArg;
+                    %fields = textscan(ItemNameArg,'%s','Delimiter','.');
+                    fields = obj.getstructfieldsarray(ItemNameArg);
+                catch
+                    error('Given OPC-Tag is not Valid.')
+                end
             else
-                error('Too many or too few arguments')
+                error('Wrong number of arguments given.')
             end
             operation = obj.operation.Read;
             soapaction = obj.getsoapAction(operation);%,["ItemName",itemName]);
@@ -73,26 +67,55 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
             try
                 getfield(obj.opctypes,fields{1}{:})
             catch
-                obj.opctypes=makestructentry(obj,obj.opctypes,itemName,type)
+                obj.opctypes=makestructentry(obj,obj.opctypes,itemName,type);
             end
         end
         
-        function r = write(obj,itemName,value)
+        function r = write(obj,itemNameArg,value)
             % In order to make the write function we need to know what
             % types are accepted by the device. Lets use the structure
             % created from browse to get a similar structure but with the
             % accepted types for every element.
+            [istype,opctype] = obj.checktype(value,itemNameArg);
+            if istype
+                xmlvalue = obj.converttype(value,opctype);
+                if (nargin == 3)
+                    try
+                        itemName = itemNameArg;
+                        %fields = textscan(ItemNameArg,'%s','Delimiter','.');
+                        fields = obj.getstructfieldsarray(itemNameArg);
+                    catch
+                        error('Given OPC-Tag is not Valid.')
+                    end
+                else
+                    error('Wrong number of arguments given.')
+                end
+                operation = obj.operation.Write;
+                soapaction = obj.getsoapAction(operation);%,["ItemName",itemName]);
+                soapaction = obj.soapactionchangeParameters(soapaction,"Items",["ItemName",itemName]);
+                %soapaction = obj.soapactionchangeParameters(soapaction,"Value",["xsi:type",opctype]);
+                soapaction = obj.soapactionchangeValue(soapaction,xmlvalue,opctype);
+                options = obj.getWeboptions(operation);
+                response = webwrite(obj.url,soapaction,options);
+            else
+                error('wrong type')
+            end
             
-            r = [itemName,value]
+            r = "OK";%xmlwrite(response);
         end
 
         function r = browse(obj,ItemNameArg)
+            % if no browse is performed and the opctags struct is empty
+            % there must be a fallback procedure where the hardcoded opc
+            % tags are used to poll the opc device
+            
             if (nargin == 1)
                 itemName = [""];
             elseif (nargin == 2)
                 itemName = ItemNameArg;
-                fields = textscan(ItemNameArg,'%s','Delimiter','.');
-                fields = matlab.lang.makeValidName(fields{1});
+                %fields = textscan(ItemNameArg,'%s','Delimiter','.');
+                %fields = matlab.lang.makeValidName(fields{1});
+                fields = obj.getstructfieldsarray(ItemNameArg);
             else
                 error('Too many or too few arguments')
             end
@@ -105,13 +128,13 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
             elements = response.getElementsByTagName("Elements");
             for i = 1:elements.getLength
                 if (elements.item(i-1).getAttributes.getNamedItem("IsItem").getValue) == 'true'
-                    name = char(elements.item(i-1).getAttributes.getNamedItem("Name").getValue)
+                    name = char(elements.item(i-1).getAttributes.getNamedItem("Name").getValue);
                     value = string(elements.item(i-1).getAttributes.getNamedItem("ItemName").getValue);
                     if itemName == ""
                         obj.opctags.(name) = value;
                     else
                         fieldnames = fields;
-                        fieldnames{end+1,:} = matlab.lang.makeValidName(name)
+                        fieldnames{end+1,:} = matlab.lang.makeValidName(name);
                         obj.opctags = setfield(obj.opctags,fieldnames{:},value);
                     end
                 elseif (elements.item(i-1).getAttributes.getNamedItem("HasChildren").getValue) == 'true'
@@ -124,13 +147,55 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
         
         % Followed by subroutines used by all requests
         %r = makestructentry(obj,struct,ItemNameArg,name,value)
+        %[r, opctype]=checktype(obj,value,opctag)
+        function [r, opctype] = checktype(obj,value,opctag)
+            
+            try
+                fields = textscan(opctag,'%s','Delimiter','.');
+            catch
+                error('Given OPC-Tag is not Valid.')
+            end
+            
+            try
+                opctype = string(getfield(obj.opctypes,fields{1}{:}));
+            catch %if not cached read from opc device and cache it
+                obj.read(opctag)
+                try
+                    opctype = string(getfield(obj.opctypes,fields{1}{:}));
+                catch
+                    error('No OPC-Type cached')
+                end
+                
+            end
+            
+            try
+                foundtype = false;
+                
+                for i = 1:length(obj.types)
+                    if (strcmp(opctype,obj.types(i,1)) & isa(value,obj.types(i,2)))
+                        foundtype = true;
+                    end
+                end
+                if foundtype
+                    r = true;
+                else
+                    r = false;
+                end
+            catch
+                error('Error')
+            end
+        end
+            
         function r = makestructentry(obj,struct,ItemNameArg,name,value)
-            fields = textscan(ItemNameArg,'%s','Delimiter','.');
-            fields = matlab.lang.makeValidName(fields{1});
-            fieldname = fields;
+            %fields = textscan(ItemNameArg,'%s','Delimiter','.');
+            %fields = matlab.lang.makeValidName(fields{1});
+            fields = obj.getstructfieldsarray(ItemNameArg);
             if (nargin == 4)
-                value = name;
+                value = name; %name holds the value if only 4 par are given
+                fieldname = fields;
             elseif (nargin == 5)
+                name = char(name);
+                fieldname = fields;
                 fieldname{end+1,:} = matlab.lang.makeValidName(name);
             else
                 error('Wrong number of arguments')
@@ -140,35 +205,35 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
         end
         function r = getWeboptions(obj,operation) %Create options for web
             if (obj.operationisValid(operation))
-                options = weboptions()
-                options.ContentType = 'xml'
-                headers = {'SOAPAction' strcat('http://opcfoundation.org/webservices/XMLDA/1.0/',char(operation))}
-                options.HeaderFields = headers
-                r = options
+                options = weboptions();
+                options.ContentType = 'xml';
+                headers = {'SOAPAction' strcat('http://opcfoundation.org/webservices/XMLDA/1.0/',char(operation))};
+                options.HeaderFields = headers;
+                r = options;
             else
                 error('operation is not valid')
             end
         end
 
         function r = request(obj,soapmessage,operation)
-            options = getWeboptions(operation)
-            r = webwrite(obj.url,soapmessage,options)
+            options = getWeboptions(operation);
+            r = webwrite(obj.url,soapmessage,options);
         end
             
         function r = getsoapAction(obj,operation,arg)
             if nargin <= 2
-                par = []
+                par = [];
             else
-                par = arg
+                par = arg;
             end
-            dom = obj.soapaction.(obj.operation.(operation))
+            dom = obj.soapaction.(obj.operation.(operation));
             
             if (isempty(par))
-                r = dom
+                r = dom;
             elseif (mod(length(par),2))
                 error('wrong dimensions ["item1","val1","item2,"val2",...]')
             else
-                r = obj.soapactionchangeParameters(dom,operation,par)
+                r = obj.soapactionchangeParameters(dom,operation,par);
             end
         end
         
@@ -177,15 +242,15 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
             % problems with generic opcxml devices
             % Maybe make a funtion which returns the xml so the programmer
             % can search for the tagname in question in the xml?
-            tagName = dom.getElementsByTagName("soapenv:Body").item(0).getChildNodes.item(1).getTagName
+            tagName = dom.getElementsByTagName("soapenv:Body").item(0).getChildNodes.item(1).getTagName;
             %tagName = strcat('ns:',operation)
             if isempty(par)
                 error('par not given')
             else
                 for i = 1:length(par)
-                    struct.(par(i)) = dom.getElementsByTagName(tagName).item(0).getAttribute(par(i))
+                    struct.(par(i)) = dom.getElementsByTagName(tagName).item(0).getAttribute(par(i));
                 end
-                r = struct
+                r = struct;
             end
         end
         
@@ -200,29 +265,44 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
                     for i = 1:2:length(par)
                         dom.getElementsByTagName(tagName).item(0).setAttribute(par(i),par(i+1))
                     end
-                    r = dom
+                    r = dom;
                 else
                     error('par has to be a string array ["item1","val1","item2,"val2",...]')
                 end
             end
         end
         
+        function r = soapactionchangeValue(obj,dom,value,opctype)
+            dom.getElementsByTagName('Value').item(0).setAttribute("xsi:type",opctype)
+            dom.getElementsByTagName('Value').item(0).setTextContent(value)
+            r = dom;
+        end
+        
+
+        
         function r = initOperationID(obj)
             for i = 1:length(obj.operations)
                 struct.(obj.operations(i)) = i
             end
-            r = struct
+            r = struct;
         end
         
         
         
         
     end
-%     methods(Static = true)
-%         out = dom2struct(dom)
-%         
-%         
-%     end
+    methods(Static)
+        function r = getstructfieldsarray(ItemNameArg)
+            fields = textscan(ItemNameArg,'%s','Delimiter','.');
+            fields = matlab.lang.makeValidName(fields{1});
+            r = fields;
+        end
+        function r = converttype(value,opctype)
+            % Not yet sure if more conversion is necesary. For now lets
+            % keep this silly function
+            r = string(value);
+        end
+    end
     methods(Access = protected)
 %         function r = getoperationID(obj,operation) %NOT USED now there is a operationID struct created on object initialisation
 %             if (operationisValid(operation))
@@ -241,7 +321,7 @@ classdef OPCXMLDA < handle & matlab.System %& matlab.mixin.SetGet%
             r = obj.operationID;
         end
         function r = operationisValid(obj,operation)
-            r = ismember(operation,obj.operations)
+            r = ismember(operation,obj.operations);
         end
     end
     
